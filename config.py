5dfaa161b34d09a44f269b3743e42d8a38851b60
rytrog <kumarvishal734513@gmail.com>
2026-02-02 06:02:22 +0530

    Update model name in config.py

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ import os
 
 API_KEY = os.getenv("SAMBANOVA_API_KEY")
 BASE_URL = "https://api.sambanova.ai/v1"
-MODEL_NAME = "DeepSeek-V3.1"
+MODEL_NAME = "Meta-Llama-3.1-8B-Instruct"
 
 
 # DO NOT CRASH — just warn
 if not API_KEY:
     print(" WARNING: SAMBANOVA_API_KEY is missing")
+
